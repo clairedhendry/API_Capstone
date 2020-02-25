2dd0a77441ba87b7c21815fa24c98ddc3c61d9ef
Claire Hendry <claire.d.hendry@gmail.com>
2020-02-25 13:24:12 -0500

    created data

---
+++ data.js
@@ -0,0 +1,78 @@
+// DATA
+
+const imageCategories = [
+    {
+        color: "red",
+        category:  [
+                "canyons", "sandstone", "lava", "sunset", "nature", "trees"
+            ],
+        mode: ["portrait", "landscape"]
+    },
+    {
+        color: "orange",
+        category: [
+                "Mars", "sunset", "desert", "nature", "trees"
+        ],
+        mode: ["portrait", "landscape"]
+    },
+    {
+        color: "yellow",
+        category: [
+                "desert", "flower", "beach", "nature", 
+        ],
+        mode: ["portrait", "landscape"]
+    }, 
+    {
+        color: "green",
+        category: [
+                "forest", "jungle", "meadow", "trees", "nature"
+        ],
+        mode: ["portrait", "landscape"]
+    },
+    {
+        color: "blue",
+        category: [
+                "sky", "ocean", "beach", "water", "river", "waterfall", "nature"
+        ],
+        mode: ["portrait", "landscape"]
+    },
+    {
+        color: "purple",
+        category: [
+                "sky", "cave", "stars", "nature"
+        ],
+        mode: ["portrait", "landscape"]
+    },
+    {
+        color: "black",
+        API: "NASA",
+        category: [
+                "stars", "nebula" 
+        ],
+        mode: ["portrait", "landscape"]
+    },
+    {
+        color: "black",
+        API: "Pixabay",
+        category: [
+                "stars", "dark"
+        ],
+        mode: ["portrait", "landscape"]
+    },
+    {
+        color: "grey",
+        subcolor: "black+white",
+        category: [
+            "rocks", "mountain", "cave"
+        ],
+        mode: ["portrait", "landscape"]
+    },
+    {
+        color: "white",
+        subcolor: "black+white",
+        category: [
+            "snow", "ice", "snowy+trees", "nature"
+        ],
+        mode: ["portrait", "landscape"]
+    }
+]
