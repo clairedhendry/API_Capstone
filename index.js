52b83e2d0692bbce61f6df173472ea0d7a293feb
Claire Hendry <claire.d.hendry@gmail.com>
2020-02-28 16:05:28 -0500

    basic functionality

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 
 const searchUrl = `https://pixabay.com/api/?key=15386213-fd2b415b0403776dbc63e2f69`
 
+// STATE
+
+const state = {
+    data: [],
+}
+
 
 // GENERATE ELEMENTS
 
@@ -23,31 +29,19 @@ function generateHomePage() {
       
 }
 
-// function generateHomePageImage(responseJson) {
-//     let hitNumber = responseJson.hits.length;
-//     let randomNumber = Math.floor((Math.random() * hitNumber) + 0);    
-//     let image = `url(${responseJson.hits[randomNumber].largeImageURL})`;
-//     let background = `${image} no-repeat center`
-//     $(".main-container").css({"background": background, "background-color": "lightgrey"});
-// }
-
-
-
-function generateSlideShow(responseJson) {
+function generateSlideShow(state) {
     
     $(".slideshow-section").removeClass("hidden");
     
-
-    for (let i = 0; i < responseJson.hits.length; i++) {
+    for (let i = 0; i < state.data[0].hits.length; i++) {
         $(".slideshow-section").append(
             `<div class="slides fade">
-            <img src=${responseJson.hits[i].largeImageURL} alt=${responseJson.hits[i].tags} style="width:100%"/>
+            <img src=${state.data[0].hits[i].largeImageURL} alt=${state.data[0].hits[i].tags} style="width:100%"/>
             <p>Images from <a href="https://pixabay.com"><img id="pixabayLogo" src="misc/logo.png" alt="Pixabay logo"/></a>
             </p>
             </div>`  
-            
-        )
-    }
+         )
+    } 
   }
 
 // MAIN FUNCTIONS
@@ -56,6 +50,7 @@ function generateSlideShow(responseJson) {
   let timeoutVariable;
     
 function showSlides() {
+
     let slides = document.getElementsByClassName("slides");
     for (let i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
@@ -65,9 +60,7 @@ function showSlides() {
     if (slideIndex > slides.length) {
         slideIndex = 1
     };
-    
     slides[slideIndex - 1].style.display = "block";
-    
     timeoutVariable = setTimeout(showSlides, 3000); 
     return timeoutVariable;
 }
@@ -82,9 +75,7 @@ function getRandomNumber(i) {
 function getCategory(colorName) {
     
     for (let i = 0; i < imageCategories.length; i++) {
-
         let string = imageCategories[i].color;
-
         if (string === colorName) {
             let randomNumber = getRandomNumber(i);
             let category = imageCategories[i].category[randomNumber];
@@ -97,29 +88,7 @@ function getCategory(colorName) {
 
 // FETCH CALLS
 
-
-// function getHomePageImage() {
-    
-//     const queryField = searchUrl + `&q=nature&image_type=photo&category=animals&orientation=vertical&editors_choice="true"&safesearch="true"`;
-
-//     fetch(queryField)
-//     .then(response => {
-//         if(response.ok) {
-//             return response.json();
-//         } else {
-//             throw new Error(response.statusText);
-//         }
-//     })
-//     .then(responseJson => generateHomePageImage(responseJson))
-//     .catch(err => {
-//         $("#error-message").text(`Something went wrong: ${err.message}`);
-//     });
-
-// }
-
 function getSlideshowImages(colorName, category) {
-
-    
 
     const searchCategory = `${encodeURIComponent("q")}=${encodeURIComponent(category)}`
     const searchColor = `${encodeURIComponent("colors")}=${encodeURIComponent(colorName)}`
@@ -134,8 +103,14 @@ function getSlideshowImages(colorName, category) {
             throw new Error(response.statusText);
         }
     })
-    .then(responseJson => generateSlideShow(responseJson))
+    
+    .then(function(responseJson) {
+         state.data.push(responseJson);
+         generateSlideShow(state)
+    })
+    
     .then(showSlides)
+
     .catch(err => {
         $("#error-message").text(`Something went wrong: ${err.message}`);
     });
@@ -150,14 +125,16 @@ function watchInput() {
 $(".color-picker input").on("click", function(event) {
     
     
-    // event.preventDefault();
+    state.data.length = 0;
+    
+    event.preventDefault();
     $(".homepage").addClass("hidden");
     $("#footer").addClass("invisible");
     $("#header").addClass("invisible");
    
     let colorName = $(this).attr("ID");
        
-    const category = getCategory(colorName);
+    let category = getCategory(colorName);
 
     getSlideshowImages(colorName, category)
   
@@ -168,20 +145,23 @@ $(".color-picker input").on("click", function(event) {
 function changeSlideshow() {
 
     $(".slideshow-section").on("click", function(event) {
+
+        state.data.length = 0;
         clearTimeout(timeoutVariable);
-        $(".slides").remove();
+        $(".slideshow-section").empty();
         $(".homepage").removeClass("hidden");
         $("#footer").removeClass("invisible");
         $("#header").removeClass("invisible");
 
-        watchInput();
+        
     })
 }
+
 
 
 // IMPLEMENT
 
 $(generateHomePage);
-// $(getHomePageImage);
+
 $(watchInput);
 $(changeSlideshow)
