21398e3572f6cdbf48d5ab99faec7a42f82837d2
Claire Hendry <claire.d.hendry@gmail.com>
2020-03-02 15:51:43 -0500

    style changes

--- data.js
+++ data.js
@@ -6,64 +6,53 @@ const imageCategories = [
         category:  [
                 "sunset", "nature", "trees", "animals"
             ],
-        mode: ["portrait", "landscape"]
     },
     {
         color: "orange",
         category: [
                 "rocks", "desert", "nature", "trees"
         ],
-        mode: ["portrait", "landscape"]
     },
     {
         color: "yellow",
         category: [
                 "flower", "beach", "nature", "animals" 
         ],
-        mode: ["portrait", "landscape"]
     }, 
     {
         color: "green",
         category: [
                 "forest", "jungle", "meadow", "trees", "nature", "animals"
         ],
-        mode: ["portrait", "landscape"]
     },
     {
         color: "blue",
         category: [
                 "sky", "ocean", "beach", "lakes", "waterfall", "nature"
         ],
-        mode: ["portrait", "landscape"]
     },
     {
         color: "purple",
         category: [
                 "sky", "nature", "flowers"
         ],
-        mode: ["portrait", "landscape"]
     },
     {
         color: "black",
         category: [
                 "stars", "dark", "animals"
         ],
-        mode: ["portrait", "landscape"]
     },
     {
         color: "grey",
-        subcolor: "black+white",
         category: [
             "rocks", "mountains", "cave", "animals"
         ],
-        mode: ["portrait", "landscape"]
     },
     {
         color: "white",
-        subcolor: "black+white",
         category: [
             "snow", "ice", "snowy+trees", "nature"
         ],
-        mode: ["portrait", "landscape"]
     }
 ]
